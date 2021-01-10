33adbcbb37425c2a7f62aa820f80664bca4599df
Ahmed Ghalab <0xcodezero@gmail.com>
2021-01-10 04:54:46 +0200

    [Feature] Enabled only button in case of Server Name is not Empty

--- hello-world-app/src/app/servers/servers.component.ts
+++ hello-world-app/src/app/servers/servers.component.ts
@@ -8,15 +8,11 @@ import { ServerComponent } from '../server/server.component';
 })
 export class ServersComponent implements OnInit {
   serversList: Array<ServerComponent>;
-  isEnabled: boolean = false;
   newToAddServerName : string = "";
   constructor() { }
 
   ngOnInit(): void {
     this.serversList = new Array();
-    setTimeout(() => {
-      this.isEnabled = true;
-    }, 2000);
 
     this.serversList.push(new ServerComponent("Ahmed"));
     this.serversList.push(new ServerComponent("Zizo"));
@@ -25,10 +21,6 @@ export class ServersComponent implements OnInit {
   }
 
   onAddingServerAction(event:Event) {
-    if (this.newToAddServerName === ""){
-      alert("Server Name Can't be Empty!");
-      return;
-    }
     this.serversList.push(new ServerComponent(this.newToAddServerName));
     this.newToAddServerName = "";
   }
